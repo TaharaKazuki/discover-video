3f2847a61e10c72b2607f9ce58aa7f98f5b9a105
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-06-15 00:32:21 +0900

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <header className="flex justify-between p-5 text-sm text-gray-700">
       <div className="flex space-x-4 items-center">
-        <p>About</p>
-        <p>Store</p>
+        <p className="link">About</p>
+        <p className="link">Store</p>
       </div>
       <div className="flex space-x-4 items-center">
-        <p>Gmail</p>
-        <p>Images</p>
+        <p className="link">Gmail</p>
+        <p className="link">Images</p>
         <User />
       </div>
     </header>
