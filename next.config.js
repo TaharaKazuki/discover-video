0ff440d891dcac466af5da96cffb55d415d6320a
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-10-07 23:33:02 +0900

    refresh

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['upload.wikimedia.org', 'lh3.googleusercontent.com'],
-  },
-}
-
-module.exports = nextConfig
