540a7fa6e81d5a58f69e088ab97c15c5f88f3336
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-10-08 23:25:55 +0900

    update

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+import { useState } from 'react'
+import type { FC } from 'react'
 import Image from 'next/image'
 import styles from './card.module.scss'
 
@@ -7,18 +8,31 @@ type Props = {
   size: 'large' | 'medium' | 'small'
 }
 
-const Card: FC<Props> = ({ imgUrl, size }) => {
+const Card: FC<Props> = ({ imgUrl = '/static/clifford.webp', size = 'medium' }) => {
+  const [imgSrc, setImgSrc] = useState(imgUrl)
+
   const classMap = {
     large: styles.lgItem,
     medium: styles.mdItem,
     small: styles.smItem,
   }
 
+  const handleOnError = () => {
+    console.info('error')
+    setImgSrc('/static/clifford.webp')
+  }
+
   return (
     <div className={styles.container}>
       Card
       <div className={classMap[size]}>
-        <Image src={imgUrl} alt="image" layout="fill" className={styles.cardImg} />
+        <Image
+          src={imgSrc}
+          alt="image"
+          layout="fill"
+          onError={handleOnError}
+          className={styles.cardImg}
+        />
       </div>
     </div>
   )
